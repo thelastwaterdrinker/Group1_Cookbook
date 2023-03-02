045d462b03aeb7519998dbf049045e5f10a0a609
prdj <pedrojr_dantas@hotmail.com>
2023-03-02 16:15:00 +0100

    last touches

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function myFunction() {
-    alert('my alert');
-}
+    alert('We are simple the best ones, never forget it!!!');
+};
+
 
 //Because shareButton1 and 2 are on the index.html eventListener cannot find them and giving and error. Therefore, it does not go to shareButton3. I added ? to solve this problem. Basically, it is helping to go trough next line.
 //document.getElementById("shareButton1")?.addEventListener("click", myFunction);
@@ -20,3 +21,7 @@ let myFunctionStack = () => {
 for (let i = 0; i < elements.length; i++) {
     elements[i].addEventListener('click', myFunctionStack, false);
 }
+
+
+
+document.getElementById("aboutUs").addEventListener("click", myFunction);
